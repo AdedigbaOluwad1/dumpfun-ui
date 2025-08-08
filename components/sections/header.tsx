7fb30978b6394c4da338d56ebe08e81a7a4bdacd
Oluwadarasimi <dara.adedigba@borro.me>
2025-08-08 18:10:25 +0100

    chore: fetch sol price on page load

--- components/sections/header.tsx
+++ components/sections/header.tsx
@@ -211,6 +211,7 @@ export function Header() {
   }, [publicKey?.toString()]);
 
   useEffect(() => {
+    fetchSolPrice();
     if (!program) return;
 
     const initializeEventId = program.addEventListener(
